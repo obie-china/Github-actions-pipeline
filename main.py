13272b3066b3f9d2a0ab1c04af4be0e760e1412a
Obiechina Iyi <iyi.obiechina@gmail.com>
2024-04-24 22:34:32 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('python push_to_github.py')
---
+++ push_to_github.py
@@ -0,0 +1,39 @@
+from scraper import *
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+from git import Repo
+
+
+
+functions = [league_table,top_scorers,detail_top,player_table,all_time_table,all_time_winner_club,top_scorers_seasons,goals_per_season]
+
+def to_github(func):
+
+    file_name = func.__name__
+    file = f"{file_name}.parquet"
+    func = func()
+    
+    #Create directory if it doesn't exist
+    os.makedirs("data", exist_ok=True)
+    
+    # Convert DataFrame to Arrow Table
+    table = pa.Table.from_pandas(func)
+
+    # write table to parquet file
+    pq.write_table(table, f"data/{file}")
+
+    # push to github repo
+    repo = Repo('.')
+    repo.index.add([f"data/{file}"])
+    repo.index.commit(f"Updated {file} data")
+    origin = repo.remote(name='origin')
+    origin.push()
+
+
+    print(f"repo successfully updated")
+
+
+for items in functions:
+    to_github(items)
